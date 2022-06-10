f2c264d0c9e2bbc990396bc3a4937c50ffcecd39
Raphaella <raphaella@Raphaellas-MacBook-Air.local>
2022-06-10 12:46:28 +0100

    moved the SQL controller into the model

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -47,4 +47,18 @@ class Bookmark
     connection.exec_params("DELETE FROM bookmarks WHERE id = $1", [id])
   end
 
+  def self.update(id:, url:, title:)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_manager')
+    end
+
+    result = connection.exec_params(
+      "UPDATE bookmarks SET url = $1, title = $2 WHERE id = $3 RETURNING id, url, title;",
+      [url, title, id]
+    )
+    Bookmark.new(id: result[0]['id'], url: result[0]['url'], title: result[0]['title'])
+  end
+
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -41,4 +41,18 @@ RSpec.describe Bookmark do
       expect(Bookmark.all.length).to eq 0
     end
   end
+
+  describe '.update' do
+    it 'updates the bookmark with the given data' do
+      bookmark = Bookmark.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+      updated_bookmark = Bookmark.update(id: bookmark.id, url: 'http://www.snakersacademy.com', title: 'Snakers Academy')
+
+      expect(updated_bookmark).to be_a Bookmark
+      expect(updated_bookmark.id).to eq bookmark.id
+      expect(updated_bookmark.title).to eq 'Snakers Academy'
+      expect(updated_bookmark.url).to eq 'http://www.snakersacademy.com'
+    end
+  end
+
+
 end
